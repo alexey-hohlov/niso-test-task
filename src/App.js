9b78b831ab67f38a2f6d4494beb8706c23be56a6
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2021-12-31 18:33:18 +0700

    refactoeing - murkup and methods moved to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
-import { MainTable } from "./components";
+// API import
+import { apiCall } from "./api/API";
+
+// import reducers
+import { useDispatch, useSelector } from "react-redux";
+import { dataSlice } from "./store/reducers/DataSlice";
+
+// components import
+import { MainTable, Buttons, InputForm, LoadingBadge, DetailedData } from "./components";
+
+// import { MainTable } from "./components";
 
 function App() {
+    // states from redux store
+    const { data, isLoading, formActive, detailsShown, details } = useSelector(
+        (state) => state.dataReducer
+    );
+    
+    // reducers
+    const { setData, setLoading, setForm, setShown, setDetails } =
+        dataSlice.actions;
+    const dispatch = useDispatch();
+    
+    // get 1000 rows data
+    const handleBigData = () => {
+        dispatch(setLoading(true));
+        apiCall.getBigData().then((response) => {
+            dispatch(setLoading(false));
+            dispatch(setData(response.data));
+        });
+    };
+
+    // get 32 rows data
+    const handleSmallData = () => {
+        dispatch(setLoading(true));
+        apiCall.getSmallData().then((response) => {
+            dispatch(setLoading(false));
+            dispatch(setData(response.data));
+        });
+    };
+
+    // open form for user input
+    const handleForm = () => {
+        dispatch(setForm(true));
+    };
+
+    // show clicked item details
+    const handleDetails = (item) => {
+        dispatch(setShown(true));
+        dispatch(setDetails(item));
+    };
+
     return (
         <div className="App">
-            <MainTable />
+            <div className="container">
+                <Buttons
+                    handleBigData={handleBigData}
+                    handleSmallData={handleSmallData}
+                    handleForm={handleForm}
+                />
+
+                {formActive && <InputForm setForm={setForm} />}
+                {detailsShown && (
+                    <DetailedData details={details} setShown={setShown} />
+                )}
+
+                {isLoading ? <LoadingBadge /> : <MainTable data={data} handleDetails={handleDetails}/>}
+            </div>
         </div>
     );
 }
--- src/components/MainTable.js
+++ src/components/MainTable.js
@@ -1,95 +1,33 @@
-// API import
-import { apiCall } from "../api/API";
-
-// import reducers
-import { useDispatch, useSelector } from "react-redux";
-import { dataSlice } from "../store/reducers/DataSlice";
-
-// components import
-import { Buttons, InputForm, LoadingBadge, DetailedData } from ".";
-
-function MainTable() {
-    const { data, isLoading, formActive, detailsShown, details } = useSelector(
-        (state) => state.dataReducer
-    );
-
-    const { setData, setLoading, setForm, setShown, setDetails } =
-        dataSlice.actions;
-    const dispatch = useDispatch();
-
-    const handleBigData = () => {
-        dispatch(setLoading(true));
-        apiCall.getBigData().then((response) => {
-            dispatch(setLoading(false));
-            dispatch(setData(response.data));
-        });
-    };
-
-    const handleSmallData = () => {
-        dispatch(setLoading(true));
-        apiCall.getSmallData().then((response) => {
-            dispatch(setLoading(false));
-            dispatch(setData(response.data));
-        });
-    };
-
-    const handleForm = () => {
-        dispatch(setForm(true));
-    };
-
-    const handleDetails = (item) => {
-        dispatch(setShown(true));
-        dispatch(setDetails(item));
-    };
-
+function MainTable({ data, handleDetails }) {
     return (
-        <div>
-            <div className="container">
-                <Buttons
-                    handleBigData={handleBigData}
-                    handleSmallData={handleSmallData}
-                    handleForm={handleForm}
-                />
-
-                {formActive && <InputForm setForm={setForm} />}
-                {detailsShown && (
-                    <DetailedData details={details} setShown={setShown} />
-                )}
-
-                {isLoading ? (
-                    <LoadingBadge />
-                ) : (
-                    <table className="table table-striped me-3">
-                        <thead>
-                            <tr className="table-primary">
-                                <th scope="col">id</th>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Phone</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((item, index) => (
-                                <tr
-                                    key={index}
-                                    style={{ cursor: "pointer" }}
-                                    onClick={() => {
-                                        handleDetails(item);
-                                    }}
-                                >
-                                    <td>{item.id}</td>
-                                    <td>{item.firstName}</td>
-                                    <td>{item.lastName}</td>
-                                    <td>{item.email}</td>
-                                    <td>{item.phone}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )}
-            </div>
-        </div>
+        <table className="table table-striped me-3">
+            <thead>
+                <tr className="table-primary">
+                    <th scope="col">id</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item, index) => (
+                    <tr
+                        key={index}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => {
+                            handleDetails(item);
+                        }}
+                    >
+                        <td>{item.id}</td>
+                        <td>{item.firstName}</td>
+                        <td>{item.lastName}</td>
+                        <td>{item.email}</td>
+                        <td>{item.phone}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
 
